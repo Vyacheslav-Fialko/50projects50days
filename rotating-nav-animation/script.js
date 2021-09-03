a061198c9640a0d30268e75b58f902d57846d207
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-09-04 00:19:27 +0300

    replaced old code to my code

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -1,7 +1,5 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const circle = document.querySelector(".circle");
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
-
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+circle.addEventListener("click", () =>
+  circle.closest(".container").classList.toggle("show-nav")
+);
