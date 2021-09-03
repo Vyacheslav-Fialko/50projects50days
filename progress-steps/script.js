dfa3e3ac1b81576083669abf93a399c157e88c3c
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-09-04 00:02:16 +0300

    replaced old code to mine code

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,49 +1,47 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+const circlesCount = circles.length;
+const lastCirrclesIndex = circlesCount - 1;
 
-let currentActive = 1
+let currentActive = 0;
 
-next.addEventListener('click', () => {
-    currentActive++
+next.addEventListener("click", () => {
+  currentActive++;
+  currentActive > lastCirrclesIndex
+    ? (currentActive = lastCirrclesIndex)
+    : update();
+});
 
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
-
-    update()
-})
-
-prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
+prev.addEventListener("click", () => {
+  currentActive--;
+  currentActive < 0 ? (currentActive = 0) : update();
+});
 
 function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
+  circles.forEach((circle, idx) => {
+    if (idx <= currentActive) {
+      if (!circle.classList.contains("active")) {
+        circle.classList.add("active");
+      }
     } else {
-        prev.disabled = false
-        next.disabled = false
+      if (circle.classList.contains("active")) {
+        circle.classList.remove("active");
+      }
     }
+  });
+
+  const activesLastIndex = document.querySelectorAll(".active").length - 1;
+  const progressWidth = (activesLastIndex / lastCirrclesIndex) * 100;
+  progress.style.width = `${progressWidth}%`;
+
+  if (currentActive === 0) {
+    prev.disabled = true;
+  } else if (currentActive === lastCirrclesIndex) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
