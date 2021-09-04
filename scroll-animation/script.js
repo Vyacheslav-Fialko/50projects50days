d61ee1c703607abc0506af262fb287c4a149f43a
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-09-04 18:18:20 +0300

    changed the old code

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,19 +1,12 @@
-const boxes = document.querySelectorAll('.box')
-
-window.addEventListener('scroll', checkBoxes)
-
-checkBoxes()
-
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
-
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
-
-        if(boxTop < triggerBottom) {
-            box.classList.add('show')
-        } else {
-            box.classList.remove('show')
-        }
-    })
-}
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight / 5) * 4.5;
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    boxTop < triggerBottom
+      ? box.classList.add("show")
+      : box.classList.remove("show");
+  });
+};
+window.onload = () => checkBoxes();
+window.addEventListener("scroll", checkBoxes);
